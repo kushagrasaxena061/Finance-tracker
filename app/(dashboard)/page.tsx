ffaa6ab595ccfaea40540ea396f3c36d4c8d35b3
Kushagra Saxena <faaltuwebsitekeliye@gmail.com>
2024-05-29 23:13:44 +0530

    header complete

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
-      <div>
-        <UserButton afterSignOutUrl="/" />
-      </div>
+      <div>Dashboard page</div>
     </div>
   );
 }
